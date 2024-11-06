b9772db0b5337a8bbf2b791ab5cc2ea5bb86ddfd
shivamgchandak <shivamgchandak@gmail.com>
2024-11-06 23:37:05 +0530

    newnew

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://studynotion-1-kpad.onrender.com",
+  baseURL: "http://studynotion-1-kpad.onrender.com",
 });
 
 axiosInstance.interceptors.request.use(
